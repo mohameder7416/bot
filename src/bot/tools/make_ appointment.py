240e11553c547c227f4240f220b6550595fe4559
mohameder7416 <m.erraouan@autodealersdigital.com>
2025-02-17 12:13:27 +0100

    add make_appointement function

--- src/bot/tools/make_ appointment.py
+++ src/bot/tools/make_ appointment.py
@@ -1,12 +1,62 @@
 
+from utils.load_variables import load_variables
+import os 
+import dt 
+import jwt
+import json
+import requests
+secret_key=os.getenv("secret_key")
+PWA_API_CRM=os.getenv("PWA_API_CRM")
+
+variables=load_variables()
+lead_id_crm=variables["lead_crm_id"]
+def create_token():
+    module_name='bot'
+    payload = {
+        "iss": module_name,  # Issuer
+        "iat": dt.datetime.utcnow(),  # Issued At
+        "exp": dt.datetime.utcnow() + dt.timedelta(minutes=2),  # Expiration
+        "nbf": dt.datetime.utcnow(),  # Not Before
+        "jti": module_name,  # JWT ID
+        "sub": module_name  # Subject
+    }
+
+    # Encode the JWT
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+    # Set up headers with the JWT token
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+    return headers
+
+
+
+
 
 
 def make_appointement():
     """
-   Generate a link to make an appointment between the customer and the dealer. The link should contain available time slots when the dealer is available
+   Generate a link to make an appointment between the customer and the dealer. 
+   The link should contain available time slots when the dealer is available
     
     
     """
+    url = f"{PWA_API_CRM}/appointment/link"
+    payload = json.dumps({
+    "lead_id": int(lead_id_crm),
+    "source": "AI Bot"
+    })
+
+    # headers = {
+    #     "Authorization": f"Bearer {BToken}",
+    #     "Content-Type": "application/json",
+    # }
+    headers=create_token()
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()['result']
+
     
     
     
